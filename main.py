d28016a93b65b2305c394597849f41400ef6f909
Talha Mirza <tmirza43@gmail.com>
2022-10-16 22:26:32 -0700

    commit1

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # ******************************
 # Make your Code
 # ******************************
+
+import random
+
+numbers = []
+for i in range(10):
+    numbers.append(random.randint(0, 100))
+
+print(numbers)
+print(f"Min value: {min(numbers)}")
+print(f"Index: {numbers.index(min(numbers))}")
